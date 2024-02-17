4c6ea858fa7d9174adbf5d00a621ecb3bbf73706
yousseflearn <youssefeasylearn@gmail.com>
2024-02-17 13:21:41 -0800

    update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from "express";
 const app = express();
 app.listen(3000, () => {
-  console.log("server is running on port 3000!!");
+  console.log("server is running on port 3000!111!");
 });
